a0f843631569d8cea78b1976592e13bcceea5565
angie <amaticorena103@hotmail.com>
2022-01-03 15:09:52 -0500

    Added coffees to selector

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,17 @@
-document.addEventListener(DOMContentLoaded, function() {
-    
+document.addEventListener("DOMContentLoaded", function() {
+    const baseURL = 'https://api.sampleapis.com/coffee/hot';
+fetch(baseURL)
+  .then(resp => resp.json())
+  .then(data => addSelections(data));
+
 })
+
+function addSelections(coffees) {
+    coffees.splice(-3)
+    coffees.forEach(coffee => {
+        const select = document.querySelector('#coffees')
+        const option = document.createElement('option')
+        option.innerText = coffee.title
+        select.appendChild(option)
+    })
+}
