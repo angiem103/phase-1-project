df5535deef34b94e24e415bad640d1d3f41ae04c
angie <amaticorena103@hotmail.com>
2022-01-03 16:06:06 -0500

    modifying array

--- javascript/index.js
+++ javascript/index.js
@@ -2,16 +2,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const baseURL = 'https://api.sampleapis.com/coffee/hot';
 fetch(baseURL)
   .then(resp => resp.json())
-  .then(data => addSelections(data));
-
+  .then(data => addSelections(data))
 })
 
 function addSelections(coffees) {
-    coffees.splice(-3)
-    coffees.forEach(coffee => {
+    const coffeesArr = createNewArr(coffees)
+    
+    coffeesArr.forEach(coffee => {
         const select = document.querySelector('#coffees')
         const option = document.createElement('option')
         option.innerText = coffee.title
         select.appendChild(option)
     })
+
+}
+
+function createNewArr(coffees){
+const latte = {
+    "title": "Latte",
+    "description": "A latte or caffè latte is a milk coffee that boasts a silky layer of foam as a real highlight to the drink. A true latte will be made up of one or two shots of espresso, lots of steamed milk and a final, thin layer of frothed milk on top.",
+    "ingredients": [
+      "Espresso",
+      "Steamed Milk",
+      "Foam"
+    ],
+    "id": 2
+  }
+const modifiedCoffees = [...coffees]
+modifiedCoffees.splice(1, 0, latte)
+modifiedCoffees.splice(-3)
+modifiedCoffees[0].title = "Black Coffee"
+return modifiedCoffees
 }
